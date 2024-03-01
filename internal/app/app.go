876f890be42b6b421f049678e2a11c676a18c6e8
Cheasezz <yuradolgo@gmail.com>
2024-03-01 13:59:19 +0300

    move logic from main package to app package

---
+++ internal/app/app.go
@@ -0,0 +1,74 @@
+package app
+
+import (
+	"context"
+	"os"
+	"os/signal"
+	"syscall"
+
+	_ "github.com/lib/pq"
+
+	"github.com/Cheasezz/goTodo/internal/repository"
+	"github.com/Cheasezz/goTodo/internal/service"
+	restServer "github.com/Cheasezz/goTodo/internal/transport/rest"
+	"github.com/Cheasezz/goTodo/internal/transport/rest/handler"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+func Run() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+	
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs: %s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgressDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: os.Getenv("DB_PASSWORD"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
+	services := service.NewServices(repos)
+	handlers := handler.NewHandlers(services)
+
+	srv := new(restServer.Server)
+	go func() {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occured while running http server: %s", err.Error())
+		}
+	}()
+
+	logrus.Print("TodoApp Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Print("TodoApp Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occured on server sgutting down: %s", err.Error())
+	}
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occured on db connection close: %s", err.Error())
+	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
